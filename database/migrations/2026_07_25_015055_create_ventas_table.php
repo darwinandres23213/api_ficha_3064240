3dae716d0c40a755fa310613a1049efef957e179
miguel <miguelcruz201715@gmail.com>
2026-07-24 21:29:20 -0500

    feat/ventas

---
+++ database/migrations/2026_07_25_015055_create_ventas_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->string("numero_factura",30)->unique();
+            $table->datetime("fecha_venta",0);
+            $table->decimal("subtotal",12,2);
+            $table->decimal("descuento",12,2);
+            $table->decimal("total",12,5);
+            $table->enum("estado", ['abierta', 'pagada', 'anulada']);
+            $table->timestamp();
+
+            $table->unsignedBigInteger("cliente_id");
+            $table->unsignedBigInteger("empleado_id");
+            $table->unsignedBigInteger("mesa_id");
+            $table->unsignedBigInteger("promocion_id");
+
+            $table->foreign("cliente_id")->references("id")->on("clientes")->onDelete("cascade");
+            $table->foreign("empleado_id")->references("id")->on("empleados")->onDelete("cascade");
+            $table->foreign("mesa_id")->references("id")->on("mesas")->onDelete("cascade");
+            $table->foreign("promocion_id")->references("id")->on("promociones")->onDelete("cascade");
+        
+            
+
+            
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+
+    {
+        Schema::table('ventas', function (Blueprint $table) {
+            //
+        });
+    }
+};
