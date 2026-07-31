0538718f2c5c5c8653963cd96fd71da5eff840d5
miguelcruz201715-hue <miguelcruz201715@gmail.com>
2026-07-30 21:11:34 -0500

    feat/interfaz_venta

---
+++ app/Interfaces/BaseInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseInterface
+
+{
+    public function create(array $data);
+    public function getAll();
+    public function getByil(int $id);
+    public function update(array $data, int $id);
+    public function delete(int $id);
+    
+}
