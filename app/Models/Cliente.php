e1f555d02c0d5e2e58c40964456d8f6406b3c497
JULY RODRIGUEZ <julynata1999@gmail.com>
2026-07-30 21:19:58 -0500

    feat/interfaz_cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -3,20 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Cliente extends Model
 {
-    use HasFactory;
+    use HasFactory; //HasFactory se va a encargar de crear datos de prueba
 
-    protected $table="clientes";
+    protected $table="clientes"; // Escribimos como se llamada la tabla en la BD
 
-    protected $fillable =[
+    protected $fillable =[ //  Definimos los datos 
         'documento',
-        'nombres_cliente',
-        'apellidos_cliente',
+        'nombres',
+        'apellidos',
         'email',
         'telefono',
         'fecha_nacimiento',
         'tipo'
     ];
+
+    public function reserva()
+
+    {
+        return $this->hasMany(Reserva::class);
+    }
+
+    public function venta()
+
+    {
+        return $this->hasMany(Venta::class);
+    }
 }
