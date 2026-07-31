8ea26b06e5283398ece774d4ec7fdbd5a4f4787c
aguirredolly618-ui <aguirredolly618@gmail.com>
2026-07-30 21:39:08 -0500

    feat/creacion_de_la_interfaz

--- app/Models/categoria_producto.php
+++ app/Models/categoria_producto.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class categoria_producto extends Model
 {
-    use Hasfactory;
+    use HasFactory;
     
     protected $table="categoria_producto";
 
