01f7bba05a70ca8ef3ed64de1a55dbcc8b92f434
JULY RODRIGUEZ <julynata1999@gmail.com>
2026-07-30 21:50:12 -0500

    Update ClienteInterface.php
    prueba

--- app/Interfaces/ClienteInterface.php
+++ app/Interfaces/ClienteInterface.php
@@ -5,6 +5,6 @@ namespace App\Interfaces;
 interface ClienteInterface extends BaseInterface
 {
     public function getByName (String $name);
-    public function getByLastname (String $lastname);
+    public function getByLastname (String $lastname);//prueba
     public function getByDocument (String $document);
 }
