211624055293c0a3a35f5c14b2b6f51d288194fe
aguirredolly618-ui <aguirredolly618@gmail.com>
2026-07-31 21:20:22 -0500

    feat/repo_proveedores

---
+++ app/Repositories/ProveedorRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories;
+use App\Interfaces\ProveedorInterface;
+use App\Models\Proveedor;
+
+class ProveedorRepository extends BaseRepository implements ProveedorInterface
+{
+    public function  __construct(Proveedor $proveedor)
+    {
+        parent::__construct($proveedor);
+    }
+
+    public function getByContacto(string $contacto)
+    {
+        $proveedores = $this->model->where("contacto",$contacto)
+                                    ->get();
+
+        if($proveedores->empty())
+            {
+                return null;
+
+            }
+
+        return $proveedores;
+    }
+
+    public function  getByemail(string $email)
+    {
+        $proveedores = $this ->model->where("email",$email)
+                                    ->get();
+
+       if($proveedores->empty())
+        {
+            return null;
+        }
+
+        return $proveedores;
+    }
+
+    public function getByDireccion(string $direccion)
+    {
+        $proveedores = $this -> model->where("direccion",$direccion)
+                                    ->get();
+
+        if($proveedores->empty())
+        {
+            return null;
+        }
+        return $proveedores;
+    }
+}
