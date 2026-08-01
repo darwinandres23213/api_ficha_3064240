e41c92a203ba0512be72c4f26defb0c88dce2277
artista <juliangomez.sena.class@gmail.com>
2026-07-31 20:48:44 -0500

    feat/repository_artista

---
+++ app/Repositories/ArtistaRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BaseInterface;
+
+
+class ArtistaRepository extends BaseRepository implements ArtistaInterface
+{
+    public function _construct(Artista $ArtistaModel)
+    {
+        parent::_construct($ArtistaModel);
+    }
+
+    public function getByRealName(string $nombre_real)
+    {
+        $nombre_real = $this->model->where("nombre_real","LIKE", "%{$nombre_real}%")
+                               ->get();
+        if($nombre_real->empty())
+        {
+            return null;
+        }
+    }
+
+    public function getByEstatus(bool $estado)
+    {
+        $estado = $this->model->where("estado")
+                              ->get();
+        if($estado->empty())
+        {
+            return null;
+        }
+    }
+
+    public function getByArtisticName(string $nombre_artistico)
+    {
+        $nombre_artistico = $this->model->where("nombre_artistico","LIKE","%{$nombre_artistico}%")
+                                        ->get();
+        if($nombre_artistico->empty())
+        {
+            return null;
+        }
+    }
+
+    public function getByMusicalGenre(string $genero_musical)
+    {
+        $genero_musical = $this->model->where("genero_musical","LIKE","%{$genero_muscial}%")
+                                      ->get();
+        if($genero_muscial->empty())
+        {
+            return null;
+        }
+    }
+
+    public function getByContact(string $contacto)
+    {
+        $contacto = $this->model->where("contacto","LIKE","%{$contacto}%")
+                                ->get();
+        if($contacto->empty())
+        {
+            return null;
+        }
+    }
+
+}
