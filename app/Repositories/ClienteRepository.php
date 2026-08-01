ba63c096b746f64fdd53dcacdb113cdf40e15b2b
JULY RODRIGUEZ <julynata1999@gmail.com>
2026-07-31 20:57:26 -0500

    creacion de repositorio

---
+++ app/Repositories/ClienteRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ClienteInterface;
+use App\Model\Cliente;
+
+class ClienteRepository extends BaseRepository implements ClienteInterface
+{   public function __construct(Cliente $clienteModel)
+    {
+        parent::__construct($clienteModel);
+    }
+    public function getByName (String $name)
+    {
+        $clientes = $this->model->where('nombre', "LIKE", "%{$name}%")
+                                ->get();
+        if($clientes->Empty()) 
+        {
+        return null;
+        }
+        return $clientes;
+    }
+
+    public function getByLastname (String $lastname)
+    {
+        $clientes = $this->model->where('apellidos', "LIKE", "%{$lastname}%")
+                                ->get();
+        if($clientes->Empty()) 
+        {
+        return null;
+        }
+        return $clientes;
+    }
+
+    public function getByDocument (String $document)
+    {
+        $clientes = $this->model->where('documento', $document)
+                                ->get();
+        if($clientes->Empty()) 
+        {
+        return null;
+        }
+        return $clientes;
+    }
+}
