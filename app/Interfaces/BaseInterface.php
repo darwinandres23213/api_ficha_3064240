37548f93992f967f4fda5751476fa540436bd3f4
Darwin <darwinandresgomez3@gmail.com>
2026-07-31 19:01:24 -0500

    version libre de problemas

--- app/Interfaces/BaseInterface.php
+++ app/Interfaces/BaseInterface.php
@@ -13,15 +13,4 @@ interface BaseInterface
     public function getByil(int $id);
     public function update(array $data, int $id);
     public function delete(int $id);
-    
-
-
-interface BaseInterface
-{
-    public function create(array $data);
-    public function getAll();
-    public function getById(int $id);
-    public function update(int $id, array $data);
-    public function delete(int $id);
-
 }
