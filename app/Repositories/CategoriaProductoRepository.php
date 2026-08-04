8c5ec7f62685e42fdadc0c260ab3f0648a599bca
cristanchopt-beep <cristanchopt@gmail.com>
2026-08-03 20:29:09 -0500

    correcion categoriaproducto

---
+++ app/Repositories/CategoriaProductoRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+namespace App\Repositories;
+
+use App\Interfaces\CategoriaProductoInterface;
+use App\Models\CategoriaProducto as CategoriaProductoModel;
+
+class CategoriaProducto extends BaseRepository implements CategoriaProductoInterface
+{
+    public function __construct(CategoriaProductoModel $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function getByName(string $nombre)
+    {
+        
+        $productos = $this->model->where("nombre", $nombre)
+            ->get();
+        if ($productos->Empty()) {
+            return null;
+        }
+        return $productos;
+    }
+
+    public function getByStatus(string $estado)
+    {
+        
+        $productos = $this->model->where("estado", $estado)
+            ->get();
+        if ($categorias->Empty()) {
+            return null;
+        }
+        return $categorias;
+    }
+}
+
+
+
