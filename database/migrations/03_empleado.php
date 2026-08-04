3cc5147a34c6f2aa2055e2825701af9c5eab3008
ZULVER2025 <duvanfelipesanchezvillamil@gmail.com>
2026-08-03 21:17:33 -0500

    correcion empleado

--- database/migrations/03_empleado.php
+++ database/migrations/03_empleado.php
@@ -9,7 +9,7 @@ return new class extends Migration
     
     public function up(): void
     {
-        Schema::create('empleados', function (Blueprint $table) {
+        Schema::create('empleado', function (Blueprint $table) {
 
             $table->id();
 
@@ -36,7 +36,7 @@ return new class extends Migration
    
     public function down(): void
     {
-        Schema:: dropIfExists('empleados');
+        Schema:: dropIfExists('empleado');
         
     }
 };
