56a5d90db8527a782f36946bf554359714017963
Julian Gómez <juliangomez.sena.class@gmail.com>
2026-08-08 11:51:18 -0500

    Update ArtistaService.php

--- app/Services/ArtistaService.php
+++ app/Services/ArtistaService.php
@@ -7,56 +7,56 @@ use App\Interfaces\ArtistaInterface;
 class ArtistaService
 {
     public function __construct( //nos permite crear\inicializar un objeto
-        private ArtistaInterface $ArtistaRepository
+        private ArtistaInterface $artistaRepository
     ) {}
 
     public function list()//list permite listar todos los registros
     {
-        return $this->ArtistaRepository->all();
+        return $this->artistaRepository->all();
     }
 
     public function show (int $id)
     {
-        return $this->ArtistaRepository->find($id);
+        return $this->artistaRepository->find($id);
     }
 
     public function store(array $data) //permite crear registros
     {
-        return $this->ArtistaRepository->create($data);
+        return $this->artistaRepository->create($data);
     }
 
     public function update(int $id, array $data)
     {
-        return $this->ArtistaRepository->update($id, $data);
+        return $this->artistaRepository->update($id, $data);
     }
 
     public function destroy(int $id)
     {
-        return $this->ArtistaRepository->delete($id);
+        return $this->artistaRepository->delete($id);
     }
 
     public function getByRealName(string $nombre_real)
     {
-        return $this->ArtistaRepository->getByRealName($nombre_real);
+        return $this->artistaRepository->getByRealName($nombre_real);
     }
 
     public function getByEstatus(bool $estado)
     {
-        return $this->ArtistaRepository->getByStatus($estado);
+        return $this->artistaRepository->getByStatus($estado);
     }
 
     public function getByArtisticName(string $nombre_artistico)
     {
-        return $this->ArtistaRepository->getByArtisticName($nombre_artistico);
+        return $this->artistaRepository->getByArtisticName($nombre_artistico);
     }
 
     public function getByMusicalGenre(string $genero_musical)//
     {
-        return $this->ArtistaRepository->getByMusicalGenre($genero_musical);
+        return $this->artistaRepository->getByMusicalGenre($genero_musical);
     }
 
     public function getByContact(string $contacto)
     {
-        return $this->ArtistaRepository->getByContact($contacto);
+        return $this->artistaRepository->getByContact($contacto);
     }
 }
