2e2b15ac585048dd45b26f44c5cbb5721b99f5c3
Darwin <darwinandresgomez3@gmail.com>
2026-08-10 16:08:05 -0500

    version_estable_clase_5

--- database/migrations/03_empleado.php
+++ database/migrations/03_empleado.php
@@ -6,37 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
     public function up(): void
     {
         Schema::create('empleados', function (Blueprint $table) {
-
             $table->id();
-
             $table->foreignId('usuario_id')
                   ->nullable()
-                  ->unique();
-
-            $table->string("documento", 20)->unique();
-            $table->string("nombres", 80);
-            $table->string("apellidos",80);
-            $table->string("cargo",60);
-            $table->date("fecha_ingreso");
-            $table->decimal('salario',12,2)->nullable();
-            $table->enum("tipo",["regular","vip","corporativo"]);
+                  ->unique()
+                  ->constrained('usuarios')
+                  ->cascadeOnDelete();
+            $table->string('documento', 20)->unique();
+            $table->string('nombres', 80);
+            $table->string('apellidos', 80);
+            $table->string('cargo', 60);
+            $table->date('fecha_ingreso');
+            $table->decimal('salario', 12, 2)->nullable();
+            $table->enum('tipo', ['regular', 'vip', 'corporativo']);
             $table->timestamps();
-            $table->unsignedBigInteger("usuario_id");
-
-            $table->foreign("usuario_id")->references("id")->on("usuarios")->onDelete("cascade");
-            
-            
         });
     }
 
-   
     public function down(): void
     {
-        Schema:: dropIfExists('empleados');
-        
+        Schema::dropIfExists('empleados');
     }
 };
