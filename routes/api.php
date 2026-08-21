7ea4d96f7b13f3bed8bab01878d6fa1a3f3b285d
miguelcruz201715-hue <miguelcruz201715@gmail.com>
2026-08-20 21:52:48 -0500

    parte 3

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MesaController;
-use APP\http\controllers\VentaController;
+use App\Http\Controllers\VentaController;
 use App\Http\Controllers\ArtistaController;
 
 Route::apiResource('mesas', MesaController::class);
 Route::apiResource('artista', ArtistaController::class);
-Route::apiResource('venta', VentaController::class);
+Route::apiResource('ventas', VentaController::class);
+Route::patch('ventas/{venta}/anular', [VentaController::class, 'anular']);
