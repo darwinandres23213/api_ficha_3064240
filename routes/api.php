1cdc95bbf19e713b8186eaef4d1881995e36f899
miguelcruz201715-hue <miguelcruz201715@gmail.com>
2026-08-20 21:48:17 -0500

    crud parte 2

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MesaController;
-
+use APP\http\controllers\VentaController;
 use App\Http\Controllers\ArtistaController;
 
 Route::apiResource('mesas', MesaController::class);
 Route::apiResource('artista', ArtistaController::class);
+Route::apiResource('venta', VentaController::class);
